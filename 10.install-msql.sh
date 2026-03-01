40b45f2c2615e23b54c4c4d1195178a1f5889204
gaganamsharath <sharath2245@gmail.com>
2026-03-01 15:45:40 +0530

    special vars

---
+++ 10.install-msql.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Error:: Please run the script with root access "
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access"
+fi
+
+dnf install mysql -y
+ 
+if [ $? -eq 0 ]
+then
+    echo "Instalation MySQL is installed"
+else
+    echo "Installization MySQL is Failed"
+    exit 1
+fi
