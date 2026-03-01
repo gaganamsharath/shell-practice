3f5d04dde34695aa3a9d88aefa23e4421324b8bb
gaganamsharath <sharath2245@gmail.com>
2026-03-01 16:35:57 +0530

    special vars

--- 10.install-msql.sh
+++ 10.install-msql.sh
@@ -9,6 +9,16 @@ then
 else
     echo "You are running with root access"
 fi
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo "Instalation $2 is installed"
+    else
+        echo "Installization $2 is Failed"
+        exit 1
+    fi
+
+}
 
 dnf list installed mysql
 
@@ -16,14 +26,29 @@ if [ $? -ne 0 ]
 then
     echo "MySQL is not installed ....going to install it"
     dnf install mysql -y
- 
-if [ $? -eq 0 ]
-then
-    echo "Instalation MySQL is installed"
-else
-    echo "Installization MySQL is Failed"
-    exit 1
-fi
+    VALIDATE $? "mysql"
 else
     echo "MySQL is already installed...Nothing to do"
+fi
 
+dnf list installed python3
+
+if [ $? -ne 0 ]
+then
+    echo "python3 is not installed ....going to install it"
+    dnf install python3 -y
+    VALIDATE $? "python"
+else
+    echo "python3 is already installed...Nothing to do"
+fi
+
+dnf list installed nginx
+
+if [ $? -ne 0 ]
+then
+    echo "ngnix is not installed ....going to install it"
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo "nginx is already installed...Nothing to do"
+fi
