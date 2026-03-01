1a824424c6a951f48a701c46f75b0e6d45e7d18b
gaganamsharath <sharath2245@gmail.com>
2026-03-01 16:12:21 +0530

    special vars

--- 10.install-msql.sh
+++ 10.install-msql.sh
@@ -10,7 +10,12 @@ else
     echo "You are running with root access"
 fi
 
-dnf install mysql -y
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed ....going to install it"
+    dnf install mysql -y
  
 if [ $? -eq 0 ]
 then
@@ -19,3 +24,6 @@ else
     echo "Installization MySQL is Failed"
     exit 1
 fi
+else
+    echo "MySQL is already installed...Nothing to do"
+
